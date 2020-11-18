72738d4125d141ca4ea3c009ca7de0a877eade18
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-11-18 18:34:20 +0200

    used react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './css/App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Current from './screens/Current';
+import Search from './screens/Search';
+import './css/App.css'
 
 function App() {
   return (
-    <div></div>
+    <React.Fragment>
+      <Router>
+        <Route exact path = "/" component = {Search} />
+        <Route exact path = "/no-redirect" component = {() => <Search redirect="same-page" />} />
+        <Route exact path = "/current" component = {Current} />
+        <Route exact path = "/current/:location" component = {Current} />
+      </Router>
+    </React.Fragment>
   );
 }
 
